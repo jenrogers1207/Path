f1897a3432e2ab652b76d10d49ca5ee3aa59df26
Jen <jen@sci.utah.edu>
2019-01-24 15:39:35 -0700

    need to download nets. fix missing semi colon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import '../styles/index.scss';
+import * as d3 from 'D3';
 
 var neoAPI = require('./neo4jLoader.js');
+var search = require('./search.js');
 
-neoAPI.findInGraph('MATCH (n:Answer) RETURN n');
+d3.select('.search-icon').on('click', () => search.searchById());
+//neoAPI.findInGraph('MATCH (n:Answer) RETURN n');
