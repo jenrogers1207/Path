056a4a968e4411d32c7e4c9522562a7ef9621df4
Jen <jen@sci.utah.edu>
2019-01-28 14:51:01 -0700

    LINKS!

--- src/scripts/graphRender.js
+++ src/scripts/graphRender.js
@@ -2,7 +2,7 @@ import '../styles/index.scss';
 import * as d3 from 'D3';
 
 export function drawGraph(data) {
-    console.log(data);
+    console.log(data.nodes);
 
     let canvas = d3.select('#graph-render').select('.graph-canvas'),
         width = +canvas.attr("width"),
@@ -14,35 +14,51 @@ export function drawGraph(data) {
         .force("x", d3.forceX(width / 2).strength(.05))
         .force("y", d3.forceY(height / 2).strength(.05))
         .force("charge", d3.forceManyBody().strength(-100))
-        .force('center', d3.forceCenter(200, 200))
+        .force('center', d3.forceCenter(300, 250))
         .force("link", d3.forceLink().distance(50).strength(1));
 
-    let node = canvas.select('.nodes').selectAll('g').data(data);
+    var link = canvas.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(data.links)
+        .enter().append("line")
+        .attr("stroke-width", 1)
+        .attr('stroke', '#9999');
 
-
+    let node = canvas.select('.nodes').selectAll('g').data(data.nodes);
 
     let nodeEnter = node
-        .enter().append("g").classed('circle-group', true);
+        .enter().append("g")
+        .attr("class", d => {
+            return "node " + d.label;
+        });
+    // .classed('circle-group', true);
 
     let circles = nodeEnter.append('circle')
-        .attr("r", radius - .75)
+        //.attr("r", radius - .75)
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
 
-    let labels = nodeEnter.append('text').text(d => d[0].properties.name).attr("transform", "translate(-18,2)");
+    let labels = nodeEnter.append('text').text(d => d.title).attr("transform", "translate(-18,2)");
 
     node = nodeEnter.merge(node);
     node.exit().remove();
 
+
+
+
     node.on('click', (d) => {
-        console.log(d[0].properties);
+        console.log(d.title);
     });
 
     simulation
-        .nodes(data)
+        .nodes(data.nodes)
         .on("tick", ticked);
+
+    simulation.force("link")
+        .links(data.links);
 
     function dragstarted(d) {
         if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -62,7 +78,15 @@ export function drawGraph(data) {
     }
 
     function ticked() {
-
+        link.attr("x1", d => {
+            return d.source.x;
+        }).attr("y1", d => {
+            return d.source.y;
+        }).attr("x2", d => {
+            return d.target.x;
+        }).attr("y2", d => {
+            return d.target.y;
+        });
         node
             .attr("transform", function(d) { return "translate(" + d.x + ", " + d.y + ")"; });
 
--- src/scripts/neo4jLoader.js
+++ src/scripts/neo4jLoader.js
@@ -1,5 +1,6 @@
 var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost:11001", neo4j.auth.basic("neo4j", "1234"));
+var _ = require('lodash');
 
 export async function addToGraph(query, type) {
     let command = 'CREATE (n:' + type + ' {name:"' + query + '"})';
@@ -46,15 +47,43 @@ export function setNodeProperty(name, prop, propValue) {
 
 export async function getGraph() {
     console.log('getting graph');
-    let command = 'MATCH (n) RETURN n ';
+    let command = 'MATCH (g:Gene)-[p:path]->(a:Pathway) \
+    RETURN g.name AS gene, collect(a.name) AS pathway';
+    //(a)-[p:path]->(b)
+
     var session = driver.session();
 
     return session
         .run(command)
         .then(function(result) {
-            let nodes = result.records.map(r => r._fields);
+            // console.log(result);
             session.close();
-            return nodes;
+
+            //  let nodes = result.records.map(r => r._fields);
+            // console.log(nodes);
+            var nodes = [],
+                rels = [],
+                i = 0;
+            result.records.forEach(res => {
+                nodes.push({ title: res.get('gene'), label: 'gene' });
+                var target = i;
+                i++;
+
+                res.get('pathway').forEach(name => {
+                    var path = { title: name, label: 'pathway' };
+                    var source = _.findIndex(nodes, path);
+                    if (source == -1) {
+                        nodes.push(path);
+                        source = i;
+                        i++;
+                    }
+                    rels.push({ source, target });
+                });
+            });
+            console.log({ nodes, links: rels });
+            return { nodes, links: rels };
+            // });
+            //    return nodes;
         })
         .catch(function(error) {
             console.log(error);
